941982ac93b75c36d3e3bf53f8f0c114751a9bac
Caio Lima <caiojlimah@gmail.com>
2022-03-23 22:46:30 -0300

    linkando aplicação com banco de dados na nuvem

--- config/config.js
+++ config/config.js
@@ -4,22 +4,22 @@ module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: 'postgres',
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: 'postgres',
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: 'postgres',
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
 };
